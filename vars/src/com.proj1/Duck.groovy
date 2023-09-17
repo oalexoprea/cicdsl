8df57a74be08ef46bc9964b84ae648f086e736bc
BuildTools <unconfigured@null.spigotmc.org>
2023-09-18 02:21:16 +0300

    use obj

---
+++ vars/src/com.proj1/Duck.groovy
@@ -0,0 +1,15 @@
+/**
+* Hero is the main entity we'll be using to . . .
+*
+* Please see the {@link com.baeldung.javadoc.Person} class for true identity
+* @author Captain America
+*
+
+*/
+class Duck implements IFly{
+
+    void fly() {
+        echo('Fly')
+    }
+
+}
---
+++ vars/src/com.proj1/IFly.groovy
@@ -0,0 +1,12 @@
+/**
+* Hero is the main entity we'll be using to . . .
+*
+* Please see the {@link com.baeldung.javadoc.Person} class for true identity
+* @author Captain America
+*
+*/
+interface IFly {
+
+    void fly()
+
+}
